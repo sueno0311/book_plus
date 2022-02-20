4a46e7d3f05375d7fbb0c31f2a2e36126418ce6a
sueno0311 <przf7ue0@gmail.com>
2022-02-20 17:11:29 +0900

    fix

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -12,4 +12,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head]
+  end
 end
